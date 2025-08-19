933ce2e9d2bdf7fe9340201af45cd2d4d7792e6f
Koru <git@koru.com>
2025-08-19 18:13:54 +0200

    Add MIT License

--- src/include/color.h
+++ src/include/color.h
@@ -23,15 +23,5 @@
 #define BRIGHT_CYAN     "\033[96m"
 #define BRIGHT_WHITE    "\033[97m"
 
-// Background Colors
-#define BG_BLACK    "\033[40m"
-#define BG_RED      "\033[41m"
-#define BG_GREEN    "\033[42m"
-#define BG_YELLOW   "\033[43m"
-#define BG_BLUE     "\033[44m"
-#define BG_MAGENTA  "\033[45m"
-#define BG_CYAN     "\033[46m"
-#define BG_WHITE    "\033[47m"
-
 // Function prototype for get_color
 const char *get_color(const char *color);
